df5d91cee20980d58e454e4ff0b056ed8e782b6b
cuocsongthinhvuong8868-sketch <cuocsongthinhvuong8868@gmail.com>
2026-05-16 09:29:56 +0700

    12

--- test_glob.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-from config import DATA_LAKE
-
-print('DATA_LAKE.glob:', list(DATA_LAKE.glob("daily_cache/feargreed_*.txt")))
-print('(DATA_LAKE / "daily_cache").glob:', list((DATA_LAKE / "daily_cache").glob("feargreed_*.txt")))
